c83222f61df5264303a5badc6c2738753965ef68
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-05-23 22:36:00 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#0A84FF',
+      'primary-hover': '#007AFF',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'gray-1': '#F2F2F7',
+      'gray-2': '#E5E5EA',
+      'gray-3': '#D1D1D6',
+      'gray-4': '#C7C7CC',
+      'gray-5': '#AEAEB2',
+      'gray-6': '#8E8E93',
+      'gray-7': '#636366',
+      'gray-8': '#48484A',
+      'gray-9': '#3A3A3C',
+      'gray-10': '#2C2C2E',
+      'gray-11': '#1C1C1E'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
