5e2a979eee1804974b4528ff9a493caa565584a7
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-05-25 19:05:35 +0800

    Add files via upload

--- components/dashboard/Data.js
+++ components/dashboard/Data.js
@@ -1,1 +1,21 @@
+import { ChartPieIcon, DocumentTextIcon, ShieldCheckIcon } from '@heroicons/react/outline';
 
+const data = [
+  {
+    title: 'Dashboard',
+    icon: <ChartPieIcon />,
+    link: '/',
+  },
+  {
+    title: 'Applications',
+    icon: <DocumentTextIcon />,
+    link: '/admin/applications',
+  },
+  {
+    title: 'Security',
+    icon: <ShieldCheckIcon />,
+    link: '/admin/security',
+  },
+];
+
+export default data;
---
+++ components/dashboard/SidebarS.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { ChartPieIcon, DocumentTextIcon, ShieldCheckIcon } from "@heroicons/react/outline";
+
+function SidebarS() {
+  return (
+    <div className="md:w-3/12 w-6/12 h-screen">
+      <div className="p-4 space-y-14">
+        <div className="space-y-4">
+          <h1 className="text-primary text-lg font-semibold">Menu</h1>
+          {/* dashboard */}
+          <Link href="/dashboard">
+            <a>
+              <div className="flex p-3 text-black space-x-4 hover:bg-primary rounded-lg hover:text-white cursor-pointer transition duration-150 mt-2">
+                <ChartPieIcon className="h-6 mr-4" />
+              Dashboard
+              </div>
+            </a>
+          </Link>
+          
+          {/* application */}
+          <Link href="/dashboard/application">
+            <a>
+              <div className="flex p-3 text-black space-x-4 hover:bg-primary rounded-lg hover:text-white cursor-pointer transition duration-150 mt-2">
+                <DocumentTextIcon className="h-6 mr-4" />
+                Application
+              </div>
+            </a>
+          </Link>
+
+          {/* user */}
+          <Link href="/dashboard/security">
+            <a>
+              <div className="flex p-3 text-primary space-x-4 hover:bg-primary rounded-lg hover:text-white cursor-pointer transition duration-150 mt-2">
+                <ShieldCheckIcon className="h-6 mr-4" />
+                Security
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SidebarS;
